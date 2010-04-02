b67d7278fc28f015a2919f364d3342e6dc1d4f69
Robert Wallhead <rwallhead@gmail.com>
2010-04-02 20:34:26 +1100

    Working face recognition!

--- face-detect.py
+++ face-detect.py
@@ -18,15 +18,18 @@ def detect(image):
     cv.EqualizeHist(grayscale, grayscale)
 
     # detect objects
-    cascade = cv.Load('haarcascade_frontalface_alt.xml', (1,1))
-    faces = cv.HaarDetectObjects(grayscale, cascade, storage, 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING, (50, 50))
+    cascade = cv.Load('haarcascade_frontalface_alt.xml')
+    faces = cv.HaarDetectObjects(grayscale, cascade, storage, 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING)
 
     if faces:
-        print 'face detected!'
         for i in faces:
-            cv.Rectangle(image, cv.Point( int(i.x), int(i.y)),
-                         cv.Point(int(i.x + i.width), int(i.y + i.height)),
-                         cv.RGB(0, 255, 0), 3, 8, 0)
+            cv.Rectangle(image,
+                         (i[0][0], i[0][1]),
+                         (i[0][0] + i[0][2], i[0][1] + i[0][3]),
+                         (0, 255, 0),
+                         3,
+                         8,
+                         0)
 
 if __name__ == "__main__":
 #    print "OpenCV version: %s (%d, %d, %d)" % (cv.VERSION,
