1471b628a30a7bf680e7eba91cd141e6cdf54c80
Robert Wallhead <rwallhead@gmail.com>
2010-04-08 17:04:46 +1000

    Now uses diff from original shot for better filtering

--- swipe-detect.py
+++ swipe-detect.py
@@ -8,12 +8,34 @@
 import cv
 
 
-BACKGROUND_GS = None
+def get_img(capture):
+    img = cv.QueryFrame(capture)
+    return img
+
+def get_hands(image):
+    """ Returns the hand as white on black. Uses value in HSV to determine
+        hands."""
+    size = cv.GetSize(image)
+    hsv = cv.CreateImage(size, 8, 3)
+    hue = cv.CreateImage(size, 8, 1)
+    sat = cv.CreateImage(size, 8, 1)
+    val = cv.CreateImage(size, 8, 1)
+    hands = cv.CreateImage(size, 8, 1)
+    cv.CvtColor(image, hsv, cv.CV_BGR2HSV)
+    cv.Split(hsv, hue, sat, val, None);
+    return val
+
+def get_diff(image1, image2):
+    """ Returns the diff. """
+    size = cv.GetSize(image)
+    diff = cv.CreateImage(size, 8, 1)
+    cv.AbsDiff(image1, image2, diff)
+    cv.Threshold(diff, diff, 100, 255, cv.CV_THRESH_BINARY)
+    cv.Smooth(diff, diff, smoothtype=cv.CV_GAUSSIAN, param1=13, param2=13)
+    cv.Threshold(diff, diff, 100, 255, cv.CV_THRESH_BINARY)
+    return diff
 
 if __name__ == "__main__":
-
-    # create windows
-    cv.NamedWindow('Live', cv.CV_WINDOW_AUTOSIZE)
 
     #set up connection to camera
     capture = cv.CaptureFromCAM(0)
@@ -24,36 +46,16 @@ if __name__ == "__main__":
     cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 320)
     cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
 
-    #get background of keyboard
-    if not BACKGROUND_GS:
-        discard = cv.QueryFrame(capture)
-        import time
-        time.sleep(5)
-        BACKGROUND = cv.QueryFrame(capture)
-        image_size = cv.GetSize(BACKGROUND)
-        BACKGROUND_GS = cv.CreateImage(image_size, 8, 1)
-        cv.Flip(BACKGROUND, None, 1)
-        cv.CvtColor(BACKGROUND, BACKGROUND_GS, cv.CV_BGR2GRAY)
-        cv.EqualizeHist(BACKGROUND_GS, BACKGROUND_GS)
+    bg = get_hands(get_img(capture))
 
     while 1:
+        image = get_img(capture)
+        hands = get_hands(image)
+        valdiff = get_diff(bg, hands)
 
-        live = cv.QueryFrame(capture)
-        image_size = cv.GetSize(live)
-        cv.Flip(live, None, 1)
-        live_gs = cv.CreateImage(image_size, 8, 1)
-        cv.CvtColor(live, live_gs, cv.CV_BGR2GRAY)
-        cv.EqualizeHist(live_gs, live_gs)
-
-        diff = cv.CreateImage(image_size, 8, 1)
-
-        cv.AbsDiff(live_gs, BACKGROUND_GS, diff)
-
-        cv.Threshold(diff, diff, 150, 255, cv.CV_THRESH_BINARY)
-
-        cv.ShowImage('Live', diff)
+        cv.ShowImage('Live', image)
+        cv.ShowImage('Value', hands)
+        cv.ShowImage('Value Diff', valdiff)
 
         # handle events
         k = cv.WaitKey(5)
-        
-        import pdb; pdb.set_trace()
