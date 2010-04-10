44618a2ab1adf7d792a9e60dadc19b94ab82d2de
Robert Wallhead <rwallhead@gmail.com>
2010-04-10 11:51:18 +1000

    WIP on getting it to work in different light levels
    Was having trouble before, this seems to work better

--- hand-detect.py
+++ hand-detect.py
@@ -23,7 +23,7 @@ def get_hands(image):
     val = cv.CreateImage(size, 8, 1)
     hands = cv.CreateImage(size, 8, 1)
     cv.CvtColor(image, hsv, cv.CV_BGR2HSV)
-    cv.Split(hsv, hue, sat, val, None);
+    cv.Split(hsv, hue, sat, val, None)
 
     cv.ShowImage('Live', image)
     cv.ShowImage('Hue', hue)
@@ -32,22 +32,27 @@ def get_hands(image):
     cv.Threshold(hue, hue, 10, 255, cv.CV_THRESH_TOZERO) #set to 0 if <= 10, otherwise leave as is
     cv.Threshold(hue, hue, 244, 255, cv.CV_THRESH_TOZERO_INV) #set to 0 if > 244, otherwise leave as is
     cv.Threshold(hue, hue, 0, 255, cv.CV_THRESH_BINARY_INV) #set to 255 if = 0, otherwise 0
-    cv.Threshold(sat, sat, 64, 255, cv.CV_THRESH_BINARY) #set to 255 if > 64, otherwise 0
+    cv.Threshold(sat, sat, 64, 255, cv.CV_THRESH_TOZERO) #set to 0 if <= 64, otherwise leave as is
+    cv.EqualizeHist(sat, sat)
+#    cv.ShowImage('Saturation with equalised histogram', sat)
 
-    cv.ShowImage('Hue threshold', hue)
+    cv.Threshold(sat, sat, 64, 255, cv.CV_THRESH_BINARY) #set to 0 if <= 64, otherwise 255
+
     cv.ShowImage('Saturation threshold', sat)
+    cv.ShowImage('Hue threshold', hue)
 
-    cv.Mul(hue, sat, hue)
+    cv.Mul(hue, sat, hands)
 
     #smooth + threshold to filter noise
-#    cv.Smooth(hue, hue, smoothtype=cv.CV_GAUSSIAN, param1=13, param2=13)
-#    cv.Threshold(hue, hue, 100, 255, cv.CV_THRESH_BINARY)
+#    cv.Smooth(hands, hands, smoothtype=cv.CV_GAUSSIAN, param1=13, param2=13)
+#    cv.Threshold(hands, hands, 200, 255, cv.CV_THRESH_BINARY)
 
-    cv.ShowImage('Hands', hue)
+    cv.ShowImage('Hands', hands)
 
-    return hue
+    return hands
 
 if __name__ == "__main__":
+    cv.NamedWindow('Live', cv.CV_WINDOW_AUTOSIZE)
 
     #set up connection to camera
     capture = cv.CaptureFromCAM(0)
